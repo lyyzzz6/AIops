4e55f7cfbb83362bae8d66fd42048562c2742f57
lyyzzz6 <mpy1499@163.com>
2026-05-11 16:46:41 +0800

    chore: Remove sql directory from version control

--- sql/add_deleted_column.sql
+++
@@ -1,2 +0,0 @@
-USE netdata_ops;
-ALTER TABLE sys_user ADD COLUMN `deleted` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '逻辑删除';
--- sql/update_password.sql
+++
@@ -1,2 +0,0 @@
-USE netdata_ops;
-UPDATE sys_user SET password = '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iAt6Z5EHsM8lE9lBOs' WHERE id = 1;
